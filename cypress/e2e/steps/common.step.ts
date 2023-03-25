0cd666f5235af692d5bf7b645cb0e07bdaeaa316
Erik Ccanto <ccanto.erik@gmail.com>
2023-03-25 20:12:14 -0300

    feat: add static code checkers

--- cypress/e2e/steps/common.step.ts
+++ cypress/e2e/steps/common.step.ts
@@ -1,5 +1,5 @@
 import { Then } from '@badeball/cypress-cucumber-preprocessor'
 
 Then('I see {string} in the title', (title: string) => {
-  cy.title().should('include', title * 2)
+  cy.title().should('include', title)
 })
